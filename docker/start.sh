6e69d0276a7f3d34b1de0eaf78bdd7d5e753a03c
Mohammed Maheen Afzal <>
2026-05-20 16:37:39 +0530

    Add Docker deployment config

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Cache config for production
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Run migrations
 php artisan migrate --force
 
-# Start services
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+php-fpm -D
+
+nginx
+
+supervisord -c /etc/supervisor/conf.d/supervisord.conf
